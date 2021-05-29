abaa745838909b91c64a99d7d697c39ce44790fc
Dion <d.trilsbeek@gmail.com>
2021-05-29 13:55:27 +0200

    refactor

--- RabbitMQEventBus/Extension/DependencyInjection/ServiceCollectionExtensions.cs
+++ RabbitMQEventBus/Extension/DependencyInjection/ServiceCollectionExtensions.cs
@@ -12,49 +12,46 @@ using RabbitMQEventbus.Extension.FunctionProvider;
 
 namespace RabbitMQEventbus.Extension.DependencyInjection
 {
-    public static class ServiceCollectionExtensions
-    {
-        private static readonly string configurationFileName = @"RabbitMQEventbus.json";
-        private static readonly string configurationFileNameProduction = @"RabbitMQEventbus.Production.json";
+	public static class ServiceCollectionExtensions
+	{
+		private static readonly string configurationFileName = @"RabbitMQEventbus.json";
+		private static readonly string configurationFileNameProduction = @"RabbitMQEventbus.Production.json";
 
-        public static IServiceCollection AddRabbitMQEventbus(this IServiceCollection services, IWebHostEnvironment env)
-        {
-            var configFileName = env.IsProduction() ? configurationFileNameProduction : configurationFileName;
-            if (!File.Exists(configFileName))
-            {
-                configFileName = configurationFileName;
-            }
-            
-            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, configFileName);
-            var file = File.ReadAllText(path);
-            var config = JsonConvert.DeserializeObject<RabbitMQEventbusConfiguration.RabbitMQEventbusConfiguration>(file);
+		public static IServiceCollection AddRabbitMQEventbus(this IServiceCollection services, IWebHostEnvironment env)
+		{
+			var configFileName = env.IsProduction() ? configurationFileNameProduction : configurationFileName;
+			if (!File.Exists(configFileName))
+			{
+				configFileName = configurationFileName;
+			}
 
-            services.AddSingleton<IConnectionFactory>(s =>
-            {
-                return new ConnectionFactory()
-                {
-                    HostName = config.Hostname,
-                    UserName = config.Username,
-                    Password = config.Password,
-                    Port = config.Port
-                };
-            });
+			string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty,
+				configFileName);
+			var file = File.ReadAllText(path);
+			var config =
+				JsonConvert.DeserializeObject<RabbitMQEventbusConfiguration.RabbitMQEventbusConfiguration>(file);
 
-            services.AddSingleton<IPersistentConnection>(s =>
-            {
-                return new PersistentConnection(s.GetRequiredService<IConnectionFactory>(),
-                                                s.GetRequiredService<ILogger<PersistentConnection>>());
-            });
+			services.AddSingleton<IConnectionFactory>(s => new ConnectionFactory()
+			{
+				HostName = config.Hostname,
+				UserName = config.Username,
+				Password = config.Password,
+				Port = config.Port
+			});
 
-            services.AddSingleton<IRabbitMQEventbus>(s =>
-            {
-                return new Eventbus.RabbitMQEventbus(config,
-                                            s.GetRequiredService<IPersistentConnection>(),
-                                            s.GetRequiredService<IRabbitMQFunctionProvider>(),
-                                            s.GetRequiredService<ILogger<Eventbus.RabbitMQEventbus>>());
-            });
+			services.AddSingleton<IPersistentConnection>(s => new PersistentConnection(
+					s.GetRequiredService<IConnectionFactory>(),
+					s.GetRequiredService<ILogger<PersistentConnection>>()
+				)
+			);
 
-            return services;
-        }
-    }
+			services.AddSingleton<IRabbitMQEventbus>(s => new Eventbus.RabbitMQEventbus(
+				config,
+				s.GetRequiredService<IPersistentConnection>(),
+				s.GetRequiredService<IRabbitMQFunctionProvider>(),
+				s.GetRequiredService<ILogger<Eventbus.RabbitMQEventbus>>()));
+
+			return services;
+		}
+	}
 }
